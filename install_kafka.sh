f6c0806e7de4c93a33f9a3c037843c9e3e651506
Eduardo Lima Fabricio <edu.fabricio@gmail.com>
2021-10-20 16:14:24 -0300

    Fix download URL

--- install_kafka.sh
+++ install_kafka.sh
@@ -2,11 +2,7 @@
 
 path=/kafka/${KAFKA_VERSION}/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz
 
-downloadUrl=$(curl --stderr /dev/null "https://www.apache.org/dyn/closer.cgi?path=${path}&as_json=1" | jq -r '"\(.preferred)\(.path_info)"')
-
-if [[ ! $(curl -sfI "${downloadUrl}") ]]; then
-    downloadUrl="https://archive.apache.org/dist/${path}"
-fi
+downloadUrl="https://archive.apache.org/dist/${path}"
 
 wget "${downloadUrl}" -O "/tmp/kafka.tgz"
 
